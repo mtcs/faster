af393437e5c19a53ccf671240acd1979d31ffad3
Matheus C. Santos <matheuscs@gmail.com>
2014-07-04 16:35:00 -0300

    Array example fully functional!

---
+++ src/include/fastTask.h
@@ -0,0 +1,20 @@
+#ifndef LIBFASTER_FASTTASK_H
+#define LIBFASTER_FASTTASK_H
+
+
+#include "definitions.h"
+
+
+class fastTask{
+	public:
+		unsigned long int id;
+		unsigned long int srcFDD;
+		unsigned long int destFDD;
+		fddOpType	operationType;
+		unsigned int	functionId;
+		void * result;
+		size_t resultSize;
+		size_t workersFinished;
+};
+
+#endif
