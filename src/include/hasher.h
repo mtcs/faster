561b03601dbc500f9e0bce47ed63aa91541395c0
Matheus C. Santos <matheuscs@gmail.com>
2015-03-24 15:26:03 -0300

    simple hashing function

---
+++ src/include/hasher.h
@@ -0,0 +1,56 @@
+#include <string>
+
+namespace faster{
+	template <typename K>
+	class hasher{
+		private:
+			int _spectrum;
+		public:
+			hasher(int spectrum){
+				_spectrum = spectrum;
+			}
+			int get ( K key ){
+				return key % _spectrum;
+			}
+
+	};
+	template <>
+	class hasher<float>{
+		private:
+			int _spectrum;
+		public:
+			hasher(int spectrum){
+				_spectrum = spectrum;
+			}
+			int get ( float key ){
+				return ((int) key) % _spectrum;
+			}
+
+	};
+	template <>
+	class hasher<double>{
+		private:
+			int _spectrum;
+		public:
+			hasher(int spectrum){
+				_spectrum = spectrum;
+			}
+			int get ( double key ){
+				return ((long long) key) % _spectrum;
+			}
+
+	};
+	template <>
+	class hasher<std::string>{
+		private:
+			int _spectrum;
+		public:
+			hasher(int spectrum){
+				_spectrum = spectrum;
+			}
+			int get ( std::string key ){
+				return ( key[0] ) % _spectrum;
+			}
+
+	};
+}
